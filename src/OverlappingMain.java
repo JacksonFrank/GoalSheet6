044725b96d4e27250f8a02d65130a3ef9209bdba
funkeydiscoman <jacksonrfrank@gmail.com>
2017-10-12 10:54:57 -0700

    Set up 6-2
    Rectangle Windows

---
+++ src/OverlappingMain.java
@@ -0,0 +1,13 @@
+/*
+ Jackson Frank
+ File: OverlappingMain.java
+ Purpose: To use the rectangle window class
+ */
+public class OverlappingMain {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,8 @@
+/*
+ Jackson Frank
+ File: Rectangle.java
+ Purpose: to create a rectangular window that also keeps track of its position, dimensions, and z-order
+ */
+public class Rectangle {
+
+}
